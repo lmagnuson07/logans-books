4f71da3e6e3f5532a7e84897ea0ad682d4b2a115
lmagnuson07 <logmagns07@gmail.com>
2023-01-14 19:56:21 -0700

    Added employee, customer, and sale tables.

--- database_scripts/alter-tables.sql
+++ database_scripts/alter-tables.sql
@@ -1,72 +1,94 @@
 USE logans_books;
 
--- Drop constraints
-ALTER TABLE bookGenreDetail DROP CONSTRAINT fk_genre_book_genre_id;
-ALTER TABLE bookGenreDetail DROP CONSTRAINT fk_genre_book_book_id;
-ALTER TABLE bookCategoryDetail DROP CONSTRAINT fk_category_book_category_id;
-ALTER TABLE bookCategoryDetail DROP CONSTRAINT fk_category_book_book_id;
-ALTER TABLE bookEditionDetail DROP CONSTRAINT fk_edition_book_edition_id;
-ALTER TABLE bookEditionDetail DROP CONSTRAINT fk_edition_book_book_id;
-ALTER TABLE bookAuthorDetail DROP CONSTRAINT fk_author_book_author_id;
-ALTER TABLE bookAuthorDetail DROP CONSTRAINT fk_author_book_book_id;
-ALTER TABLE bookPublisherDetail DROP CONSTRAINT fk_publisher_book_publisher_id;
-ALTER TABLE bookPublisherDetail DROP CONSTRAINT fk_publisher_book_book_id;
-ALTER TABLE bookPriceDetail DROP CONSTRAINT fk_price_book_price_id;
-ALTER TABLE bookPriceDetail DROP CONSTRAINT fk_price_book_book_id;
+-- ---------------------- Drop constraints -----------------------------------------------
+-- Drop Foreign keys
+ALTER TABLE bookgenredetail DROP CONSTRAINT fk_genre_book_genre_id;
+ALTER TABLE bookgenredetail DROP CONSTRAINT fk_genre_book_book_id;
+ALTER TABLE bookcategorydetail DROP CONSTRAINT fk_category_book_category_id;
+ALTER TABLE bookcategorydetail DROP CONSTRAINT fk_category_book_book_id;
+ALTER TABLE bookeditiondetail DROP CONSTRAINT fk_edition_book_edition_id;
+ALTER TABLE bookeditiondetail DROP CONSTRAINT fk_edition_book_book_id;
+ALTER TABLE bookauthordetail DROP CONSTRAINT fk_author_book_author_id;
+ALTER TABLE bookauthordetail DROP CONSTRAINT fk_author_book_book_id;
+ALTER TABLE bookpublisherdetail DROP CONSTRAINT fk_publisher_book_publisher_id;
+ALTER TABLE bookpublisherdetail DROP CONSTRAINT fk_publisher_book_book_id;
+ALTER TABLE bookpricedetail DROP CONSTRAINT fk_price_book_price_id;
+ALTER TABLE bookpricedetail DROP CONSTRAINT fk_price_book_book_id;
 
--- Foreign Keys
--- ------ Books ----------------------------------------------------------
+ALTER TABLE employeepositiondetail DROP CONSTRAINT fk_position_employee_position_id;
+ALTER TABLE employeepositiondetail DROP CONSTRAINT fk_position_employee_employee_id;
+ALTER TABLE employeemanagerdetail DROP CONSTRAINT fk_manager_employee_manager_id;
+ALTER TABLE employeemanagerdetail DROP CONSTRAINT fk_manager_employee_employee_id;
+
+-- Drop Indexes
+ALTER TABLE book DROP INDEX i_book_title;
+ALTER TABLE employee DROP INDEX i_employee_username;
+
+-- Drop Unique
+ALTER TABLE employee DROP CONSTRAINT uc_username;
+
+-- ------ Books --------------------------------------------------------------------------
 -- 1
-ALTER TABLE bookGenreDetail ADD CONSTRAINT fk_genre_book_genre_id
-FOREIGN KEY (genre_id) REFERENCES bookGenre(id);
+ALTER TABLE bookgenredetail ADD CONSTRAINT fk_genre_book_genre_id
+FOREIGN KEY (genre_id) REFERENCES bookgenre(id);
 
-ALTER TABLE bookGenreDetail ADD CONSTRAINT fk_genre_book_book_id 
+ALTER TABLE bookgenredetail ADD CONSTRAINT fk_genre_book_book_id 
 FOREIGN KEY (book_id) REFERENCES book(id);
 -- 2
-ALTER TABLE bookCategoryDetail ADD CONSTRAINT fk_category_book_category_id
-FOREIGN KEY (category_id) REFERENCES bookCategory(id);
+ALTER TABLE bookcategorydetail ADD CONSTRAINT fk_category_book_category_id
+FOREIGN KEY (category_id) REFERENCES bookcategory(id);
 
-ALTER TABLE bookCategoryDetail ADD CONSTRAINT fk_category_book_book_id
+ALTER TABLE bookcategorydetail ADD CONSTRAINT fk_category_book_book_id
 FOREIGN KEY (book_id) REFERENCES book(id);
 -- 3
-ALTER TABLE bookEditionDetail ADD CONSTRAINT fk_edition_book_edition_id
-FOREIGN KEY (edition_id) REFERENCES bookEdition(id);
+ALTER TABLE bookeditiondetail ADD CONSTRAINT fk_edition_book_edition_id
+FOREIGN KEY (edition_id) REFERENCES bookedition(id);
 
-ALTER TABLE bookEditionDetail ADD CONSTRAINT fk_edition_book_book_id
+ALTER TABLE bookeditiondetail ADD CONSTRAINT fk_edition_book_book_id
 FOREIGN KEY (book_id) REFERENCES book(id);
 -- 4 
-ALTER TABLE bookAuthorDetail ADD CONSTRAINT fk_author_book_author_id
-FOREIGN KEY (author_id) REFERENCES bookAuthor(id);
+ALTER TABLE bookauthordetail ADD CONSTRAINT fk_author_book_author_id
+FOREIGN KEY (author_id) REFERENCES bookauthor(id);
 
-ALTER TABLE bookAuthorDetail ADD CONSTRAINT fk_author_book_book_id
+ALTER TABLE bookauthordetail ADD CONSTRAINT fk_author_book_book_id
 FOREIGN KEY (book_id) REFERENCES book(id);
 -- 5
-ALTER TABLE bookPublisherDetail ADD CONSTRAINT fk_publisher_book_publisher_id
-FOREIGN KEY (publisher_id) REFERENCES bookPublisher(id);
+ALTER TABLE bookpublisherdetail ADD CONSTRAINT fk_publisher_book_publisher_id
+FOREIGN KEY (publisher_id) REFERENCES bookpublisher(id);
 
-ALTER TABLE bookPublisherDetail ADD CONSTRAINT fk_publisher_book_book_id
+ALTER TABLE bookpublisherdetail ADD CONSTRAINT fk_publisher_book_book_id
 FOREIGN KEY (book_id) REFERENCES book(id);
 -- 6
-ALTER TABLE bookPriceDetail ADD CONSTRAINT fk_price_book_price_id
-FOREIGN KEY (price_id) REFERENCES bookPrice(id);
+ALTER TABLE bookpricedetail ADD CONSTRAINT fk_price_book_price_id
+FOREIGN KEY (price_id) REFERENCES bookprice(id);
 
-ALTER TABLE bookPriceDetail ADD CONSTRAINT fk_price_book_book_id
+ALTER TABLE bookpricedetail ADD CONSTRAINT fk_price_book_book_id
 FOREIGN KEY (book_id) REFERENCES book(id);
--- ------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------------------------------
+-- Employee/Admin --------------------------------------------------------------------------------------
+ALTER TABLE employeepositiondetail ADD CONSTRAINT fk_position_employee_position_id
+FOREIGN KEY (position_id) REFERENCES logans_books.position(id);
+
+ALTER TABLE employeepositiondetail ADD CONSTRAINT fk_position_employee_employee_id
+FOREIGN KEY (employee_id) REFERENCES employee(id);
+
+ALTER TABLE employeemanagerdetail ADD CONSTRAINT fk_manager_employee_manager_id
+FOREIGN KEY (manager_id) REFERENCES employee(id);
+
+ALTER TABLE employeemanagerdetail ADD CONSTRAINT fk_manager_employee_employee_id
+FOREIGN KEY (employee_id) REFERENCES employee(id);
 
 
+-- ----- Indexes ----------------------------------------------------------------------------------------
+-- Books
+ALTER TABLE book ADD INDEX i_book_title (title);
 
+-- Employee
+ALTER TABLE employee ADD INDEX i_employee_username (username);
 
-
-
-
-
-
-
-
-
-
-
+-- ----- Unique ----------------------------------------------------------------------------------------
+-- Employee
+ALTER TABLE employee ADD CONSTRAINT uc_username UNIQUE (username);
 
 
 
--- database_scripts/create-tables.sql
+++ database_scripts/create-tables.sql
@@ -2,19 +2,32 @@ USE logans_books;
 
 -- Drop Tables
 DROP TABLE IF EXISTS book;
-DROP TABLE IF EXISTS bookGenre;
-DROP TABLE IF EXISTS bookCategory;
-DROP TABLE IF EXISTS bookEdition;
-DROP TABLE IF EXISTS bookAuthor;
-DROP TABLE IF EXISTS bookPublisher;
-DROP TABLE IF EXISTS bookPrice;
+DROP TABLE IF EXISTS bookgenre;
+DROP TABLE IF EXISTS bookcategory;
+DROP TABLE IF EXISTS bookedition;
+DROP TABLE IF EXISTS bookauthor;
+DROP TABLE IF EXISTS bookpublisher;
+DROP TABLE IF EXISTS bookprice;
 
-DROP TABLE IF EXISTS bookGenreDetail;
-DROP TABLE IF EXISTS bookCategoryDetail;
-DROP TABLE IF EXISTS bookEditionDetail;
-DROP TABLE IF EXISTS bookAuthorDetail;
-DROP TABLE IF EXISTS bookPublisherDetail;
-DROP TABLE IF EXISTS bookPriceDetail;
+DROP TABLE IF EXISTS bookgenredetail;
+DROP TABLE IF EXISTS bookcategorydetail;
+DROP TABLE IF EXISTS bookeditiondetail;
+DROP TABLE IF EXISTS bookauthordetail;
+DROP TABLE IF EXISTS bookpublisherdetail;
+DROP TABLE IF EXISTS bookpricedetail;
+
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS position;
+DROP TABLE IF EXISTS employeepositiondetail;
+DROP TABLE IF EXISTS employeemanagerdetail;
+
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS coupon;
+DROP TABLE IF EXISTS sale;
+DROP TABLE IF EXISTS paymentcard;
+DROP TABLE IF EXISTS salepaymentcarddetail;
+DROP TABLE IF EXISTS salecoupondetail;
+DROP TABLE IF EXISTS saleitemdetail;
 
 -- --------- Books -----------------------------------------------------------------------------------
 -- 1
@@ -33,25 +46,25 @@ CREATE TABLE IF NOT EXISTS book (
     ,is_available			TINYINT			DEFAULT NULL
 );
 -- 2
-CREATE TABLE IF NOT EXISTS bookGenre (
+CREATE TABLE IF NOT EXISTS bookgenre (
 	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
     ,name					VARCHAR(255)	NOT NULL
     ,description			TEXT			DEFAULT NULL	
 );
 -- 3
-CREATE TABLE IF NOT EXISTS bookCategory (
+CREATE TABLE IF NOT EXISTS bookcategory (
 	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
     ,name					VARCHAR(255)	NOT NULL
     ,description			TEXT			DEFAULT NULL
 );
 -- 4
-CREATE TABLE IF NOT EXISTS bookEdition (
+CREATE TABLE IF NOT EXISTS bookedition (
 	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
     ,name					VARCHAR(255)	NOT NULL
     ,description			TEXT			DEFAULT NULL
 );
 -- 5
-CREATE TABLE IF NOT EXISTS bookAuthor (
+CREATE TABLE IF NOT EXISTS bookauthor (
 	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
     ,first_name				VARCHAR(255)	NOT NULL	
     ,last_name				VARCHAR(255)	NOT NULL
@@ -59,7 +72,7 @@ CREATE TABLE IF NOT EXISTS bookAuthor (
     ,email					VARCHAR(255)	DEFAULT NULL
 );
 -- 6
-CREATE TABLE IF NOT EXISTS bookPublisher (
+CREATE TABLE IF NOT EXISTS bookpublisher (
 	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY		
     ,name					VARCHAR(255)	NOT NULL	
     ,residence_id			INT				NOT NULL
@@ -70,69 +83,164 @@ CREATE TABLE IF NOT EXISTS bookPublisher (
 	,phone					VARCHAR(50)		DEFAULT NULL
 );
 -- 7 
-CREATE TABLE IF NOT EXISTS bookPrice (
+CREATE TABLE IF NOT EXISTS bookprice (
 	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY		
     ,historical_price		DECIMAL(10,4)	NOT NULL
     ,historical_price_date	DATETIME		NOT NULL
 );
 -- bridging tables
 -- 1
-CREATE TABLE IF NOT EXISTS bookGenreDetail (
+CREATE TABLE IF NOT EXISTS bookgenredetail (
 	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
     ,book_id				INT 			NOT NULL
     ,genre_id				INT 			NOT NULL
 );
 -- 2
-CREATE TABLE IF NOT EXISTS bookCategoryDetail (
+CREATE TABLE IF NOT EXISTS bookcategorydetail (
 	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
     ,book_id				INT 			NOT NULL
     ,category_id			INT 			NOT NULL
 );
 -- 3
-CREATE TABLE IF NOT EXISTS bookEditionDetail (
+CREATE TABLE IF NOT EXISTS bookeditiondetail (
 	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
     ,book_id				INT 			NOT NULL
     ,edition_id				INT 			NOT NULL
 );
 -- 4
-CREATE TABLE IF NOT EXISTS bookAuthorDetail (
+CREATE TABLE IF NOT EXISTS bookauthordetail (
 	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
     ,book_id				INT 			NOT NULL
     ,author_id				INT 			NOT NULL
 );
 -- 5
-CREATE TABLE IF NOT EXISTS bookPublisherDetail (
+CREATE TABLE IF NOT EXISTS bookpublisherdetail (
     book_id				INT 			NOT NULL
     ,publisher_id		INT 			NOT NULL
     ,published_date		DATETIME		NOT NULL
     ,PRIMARY KEY(book_id, publisher_id)
 );
 -- 6
-CREATE TABLE IF NOT EXISTS bookPriceDetail (
+CREATE TABLE IF NOT EXISTS bookpricedetail (
 	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
     ,book_id				INT 			NOT NULL
     ,price_id				INT 			NOT NULL
 );
 -- -----------------------------------------------------------------------------------------------------
 -- Employee/Admin --------------------------------------------------------------------------------------
--- CREATE TABLE IF NOT EXISTS employee (
--- 	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
---     ,first_name				VARCHAR(255)	NOT NULL
---     ,last_name				VARCHAR(255)	NOT NULL
---     ,
--- );
-
-
-
-
-
-
-
-
-
-
-
-
+-- 1
+CREATE TABLE IF NOT EXISTS employee (
+	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
+    ,first_name				VARCHAR(255)	NOT NULL
+    ,last_name				VARCHAR(255)	NOT NULL
+    ,resident_id			INT				NOT NULL
+    ,street_address			VARCHAR(255)	NOT NULL	
+    ,postal_code			VARCHAR(50)		NOT NULL
+    ,email					VARCHAR(255)	NOT NULL	
+    ,username				VARCHAR(255)	NOT NULL
+    ,hashed_password		VARCHAR(255)	NOT NULL
+    ,home_phone				VARCHAR(50)		DEFAULT NULL
+    ,cell_phone				VARCHAR(50)		DEFAULT NULL
+    ,date_hired				DATETIME		NOT NULL	DEFAULT (CURRENT_DATE)
+    ,date_released			DATETIME		DEFAULT NULL
+    ,emergency_contact_fn	VARCHAR(255)	DEFAULT NULL
+    ,emergency_contact_ln	VARCHAR(255)	DEFAULT NULL
+    ,emergency_contact_phone	VARCHAR(255)	DEFAULT NULL
+    ,emergency_contact_relation	VARCHAR(255)	DEFAULT NULL
+    ,is_admin				TINYINT			NOT NULL 	DEFAULT 0
+);
+-- 2
+CREATE TABLE IF NOT EXISTS position (
+	id						INT 			NOT NULL 	AUTO_INCREMENT	PRIMARY KEY
+    ,description			VARCHAR(255)	NOT NULL
+    ,privilege_level		TINYINT 		NOT NULL
+);
+-- Bridging tables
+-- 1
+CREATE TABLE IF NOT EXISTS employeepositiondetail (
+	id						INT 			NOT NULL 	AUTO_INCREMENT	PRIMARY KEY
+    ,employee_id			INT				NOT NULL
+    ,position_id			INT				NOT NULL
+);
+-- 2
+CREATE TABLE IF NOT EXISTS employeemanagerdetail (
+	id						INT 			NOT NULL 	AUTO_INCREMENT	PRIMARY KEY
+    ,employee_id			INT				NOT NULL
+    ,manager_id				INT 			NOT NULL
+);
+-- -----------------------------------------------------------------------------------------------------
+-- Customer - Sale --------------------------------------------------------------------------------------
+-- 1
+CREATE TABLE IF NOT EXISTS customer (
+	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
+    ,first_name				VARCHAR(255)	NOT NULL
+    ,last_name				VARCHAR(255)	NOT NULL
+    ,resident_id			INT				NOT NULL
+    ,street_address			VARCHAR(255)	NOT NULL	
+    ,postal_code			VARCHAR(50)		NOT NULL
+    ,email					VARCHAR(255)	NOT NULL	
+    ,username				VARCHAR(255)	NOT NULL
+    ,hashed_password		VARCHAR(255)	NOT NULL
+    ,home_phone				VARCHAR(50)		DEFAULT NULL
+    ,cell_phone				VARCHAR(50)		DEFAULT NULL
+    ,prefered_contact_method	VARCHAR(50)	DEFAULT NULL
+    ,security_question_one	VARCHAR(255)	NOT NULL
+    ,security_answer_one	VARCHAR(255)	NOT NULL
+    ,security_question_two	VARCHAR(255)	NOT NULL
+    ,security_answer_two	VARCHAR(255) 	NOT NULL
+);
+-- 2
+CREATE TABLE IF NOT EXISTS coupon (
+	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
+	,code					VARCHAR(255)	NOT NULL
+    ,description			TEXT			NOT NULL	
+    ,type					VARCHAR(50)		NOT NULL
+    ,discount_percentage	DECIMAL(4,4)	DEFAULT NULL
+    ,discount_dollar_value	DECIMAL(10,4) 	DEFAULT NULL
+    ,start_date				DATETIME		NOT NULL
+    ,end_date				DATETIME		NOT NULL
+);
+-- 3
+CREATE TABLE IF NOT EXISTS sale (
+	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
+    ,date					DATETIME		NOT NULL 	DEFAULT (CURRENT_DATE)
+    ,payment_type			VARCHAR(50)		NOT NULL	
+    ,tax_amount				DECIMAL(10,4)	NOT NULL
+    ,subtotal				DECIMAL(10,4)	NOT NULL
+    ,employee_id			INT				DEFAULT NULL
+    ,customer_id			INT				NOT NULL
+);
+-- 4
+CREATE TABLE IF NOT EXISTS paymentcard (
+	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
+    ,primary_account_number	VARCHAR(20)		NOT NULL
+    ,service_code			VARCHAR(10)		NOT NULL
+    ,expiration_date		DATE			NOT NULL
+    ,cardholder_name		VARCHAR(255)	NOT NULL
+	,payment_card_type		VARCHAR(50) 	NOT NULL
+    ,customer_id			INT 			NOT NULL
+);
+-- Bridging tabes
+-- 1
+CREATE TABLE IF NOT EXISTS salepaymentcarddetail (
+	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
+    ,sale_id				INT				NOT NULL
+    ,payment_card_id		INT 			NOT NULL
+);
+-- 2
+CREATE TABLE IF NOT EXISTS salecoupondetail (
+	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
+    ,sale_id				INT				NOT NULL
+    ,coupon_id				INT 			NOT NULL
+);
+-- 3
+CREATE TABLE IF NOT EXISTS saleitemdetail (
+	id						INT				NOT NULL	AUTO_INCREMENT	PRIMARY KEY
+	,selling_price			DECIMAL(10,4)	NOT NULL
+    ,qty					INT 			NOT NULL
+    ,sale_id				INT				NOT NULL
+    ,book_id				INT 			NOT NULL
+);
 
 
 
